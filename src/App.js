87aa3c9c77418f7d81c8616b0d03de6926a365fb
Omid <mail.dev.om@gmail.com>
2024-08-27 13:52:22 +0330

    jghg

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import './App.css';
-import { Box, Button } from '@mui/joy'
+import { Box, Button } from '@mui/joy';
 import Buttt from './components/button';
 
 function App() {
-  const click = async() => {
-    const resonse = await fetch('http://localhost:5005', {
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      method: 'POST',
-    })
+  const click = async () => {
+    try {
+      const response = await fetch('https://testbotuibackend.onrender.com', {
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        method: 'POST',
+      });
+      if (response.ok) {
+        const responseData = await response.json();
+        console.log('Response:', responseData);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
   };
 
   return (
